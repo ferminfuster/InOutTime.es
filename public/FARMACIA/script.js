f8579d1166860963b69a3a58083d59c0bb76777e
Fermin Fuster <ferminfuster@gmail.com>
2025-03-04 13:10:35 +0100

    [skip_ci] Deploy 2025-03-04 13:10:35

--- public/FARMACIA/script.js
+++ public/FARMACIA/script.js
@@ -3,3 +3,25 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     alert("Formulario enviado. Nos pondremos en contacto pronto.");
     this.reset();
 });
+window.onload = function() {
+    var popup = document.getElementById("popup");
+    var closeBtn = document.getElementById("closePopup");
+
+    // Mostrar el popup al cargar la página
+    popup.style.display = "block";
+
+    // Generar confeti
+    confetti();
+
+    // Cerrar el popup cuando se hace clic en la "X"
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // Cerrar el popup si se hace clic fuera del contenido
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+}
