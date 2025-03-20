89f28dc27c0a1b1e13b6caf698091b6cccb8f9a3
Fermin Fuster <ferminfuster@gmail.com>
2025-03-20 12:01:38 +0100

    [skip_ci] Deploy 2025-03-20 12:01:38

--- public/FARMACIA/script.js
+++ public/FARMACIA/script.js
@@ -3,25 +3,22 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     alert("Formulario enviado. Nos pondremos en contacto pronto.");
     this.reset();
 });
-window.onload = function() {
-    var popup = document.getElementById("popup");
-    var closeBtn = document.getElementById("closePopup");
 
-    // Mostrar el popup al cargar la página
-    popup.style.display = "block";
+        function openTab(evt, tabName) {
+            // Ocultar todas las pestañas
+            var i, tabcontent, tabbuttons;
+            tabcontent = document.getElementsByClassName("tab-content");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].classList.remove("active");
+            }
 
-    // Generar confeti
-    confetti();
+            // Remover la clase activa de todos los botones
+            tabbuttons = document.getElementsByClassName("tab-button");
+            for (i = 0; i < tabbuttons.length; i++) {
+                tabbuttons[i].classList.remove("active");
+            }
 
-    // Cerrar el popup cuando se hace clic en la "X"
-    closeBtn.onclick = function() {
-        popup.style.display = "none";
-    }
-
-    // Cerrar el popup si se hace clic fuera del contenido
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.style.display = "none";
+            // Mostrar la pestaña actual y añadir la clase activa al botón
+            document.getElementById(tabName).classList.add("active");
+            evt.currentTarget.classList.add("active");
         }
-    }
-}
